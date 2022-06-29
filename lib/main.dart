b4bd4347baddd0acf4b093c901ae83ba07ba4e53
ATSUSHIKADOWAKI <at4forfor@gmail.com>
2022-06-29 10:52:52 +0900

    テキスト追記

--- lib/main.dart
+++ lib/main.dart
@@ -95,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('あっちょ！！！！'),
+            Text('あちょっちょちょちょっちょ！！'),
             const Text(
               'You have pushed the button this many times:',
             ),
